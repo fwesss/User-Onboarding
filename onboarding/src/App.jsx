866e6749d4fe12f4a4fd42f9865e642a32912943
Westley Feller <feller.westley@gmail.com>
2019-10-10 19:45:59 -0700

    Form setup and styled

--- onboarding/src/App.jsx
+++ onboarding/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Form from './components/Form';
+import FormikLoginForm from './components/Form';
 
 
 function App() {
   return (
-    <Form />
+    <FormikLoginForm />
   );
 }
 
--- onboarding/src/components/Form.jsx
+++ onboarding/src/components/Form.jsx
@@ -1,8 +1,67 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
 
 
-const Form = () => (
-  <p>yo</p>
+const LoginForm = () => (
+  <section className="section">
+    <div className="container columns is-centered">
+
+      <Form className="column is-4">
+        <div className="field">
+          <div className="control">
+            <Field className="input" type="text" name="name" placeholder="Name" />
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <Field className="input" type="email" name="email" placeholder="Email" />
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <Field className="input" type="password" name="password" placeholder="password" />
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <label className="checkbox" htmlFor="tos">
+              <Field type="checkbox" name="tos" />
+              {' '}
+              I agree to the
+              {' '}
+              <a href="#">terms and conditions</a>
+            </label>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <button className="button is-primary" type="submit">Submit</button>
+          </div>
+        </div>
+      </Form>
+    </div>
+  </section>
 );
 
-export default Form;
+const FormikLoginForm = withFormik({
+  mapPropsToValues({
+    name, email, password, tos,
+  }) {
+    return {
+      name: name || '',
+      email: email || '',
+      password: password || '',
+      tos: tos || false,
+    };
+  },
+
+  handleSubmit(values) {
+    console.log(values);
+  },
+})(LoginForm);
+
+export default FormikLoginForm;
